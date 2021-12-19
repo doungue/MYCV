d023722b546c08ad1042200fc749b2d6908f4986
doungue <94931435+doungue@users.noreply.github.com>
2021-12-19 21:24:51 +0100

    cv

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta charset="utf-8"/>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style30.css"/>
+			<link rel="stylesheet" href="style50.css"/>
+			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>mon cv en html et css</title>
+		    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    </head>
+    <body>
+      <div class="container">
+          <div class="contact">    
+	                    <?php include "profilclass.php"; ?>
+                        </div>                 
+				        <?php include "competenceclass.php"; ?>					 					 				  				  
+          </div>
+          </div>
+          <div class="contact">
+			      <?php include "experienceclass.php"; ?>
+				  <?php include "loisirlass.php"; ?>				 
+                  <?php include "cursusclass.php"; ?>				
+                  </div>
+              </div>
+          </div>
+    </div>
+    </body>
+    </html>
